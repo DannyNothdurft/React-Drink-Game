5b45b35e0223fba5a488828d291b7e8ceef16464
Danny Nothdurft <d.nothdurft@gmail.de>
2022-05-02 13:40:49 +0200

    React App aufgeräumt. Datein gelöscht, Ordner zugefügt, Datein verschoben, Datei Format geändert

---
+++ app/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h2>Aufgeräumt</h2>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;

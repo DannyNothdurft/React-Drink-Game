086f4c6930c32294ea40a5a23d170ea18101e9ed
Danny Nothdurft <d.nothdurft@gmail.de>
2022-05-02 17:01:59 +0200

    language.json wurde angelegt. Json Datei mit Infobox verbunden. Text aus Json Datei eingefügt

--- app/src/components/Infobox.jsx
+++ app/src/components/Infobox.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
+// data
+import Language from './../data/language.json'
+
 function Infobox() {
-    
+
   return (
     <div className='infobox'>
-        <h2>Überschrift</h2>
+        <h2>{Language[0].contents.h2}</h2>
         <hr />
-        <p>Anleitung für das spiel aus JSON datei</p>
+        <p>{Language[0].contents.p1}</p>
+        <p>{Language[0].contents.p2}</p>
+        <ul>
+            <li>{Language[0].contents.l1}</li>
+            <li>{Language[0].contents.l2}</li>
+            <li>{Language[0].contents.l3}</li>
+            <li>{Language[0].contents.l4}</li>
+        </ul>
+        <p>{Language[0].contents.p3}</p>
+        <p>{Language[0].contents.p4}</p>
+        <p>{Language[0].contents.p5}</p>
     </div>
   )
 }

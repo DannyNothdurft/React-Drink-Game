28ad7bfca8f5e14a1caf04e8953a63110432bc55
Danny Nothdurft <d.nothdurft@gmail.de>
2022-05-02 16:01:11 +0200

    Button die funktion gegeben ein Div zu öffnen und zu schließen

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {FaInfoCircle} from 'react-icons/fa';
 
 function Header() {
+
+  const [ info, setInfo ] = useState(false);
+
   return (
     <div className='header-component'>
         <h1>Drink randomly</h1>
-        <button><FaInfoCircle /></button>
+        <button
+          onClick={ () => { setInfo(!info); } }        
+        >
+          <FaInfoCircle />
+        </button>
+        { info ? <div>Funktioniert</div> : null }
     </div>
   )
 }

b0819fc9282bdbedd2794bd5936ff5393277be8d
Danny Nothdurft <d.nothdurft@gmail.de>
2022-05-02 15:56:20 +0200

    Button mit einem Info Icon hinzugefügt

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import {FaInfoCircle} from 'react-icons/fa';
 
 function Header() {
   return (
     <div className='header-component'>
         <h1>Drink randomly</h1>
+        <button><FaInfoCircle /></button>
     </div>
   )
 }

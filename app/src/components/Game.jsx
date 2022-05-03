722b31833f79bfd9186e0acaf99aa0bc094e52a7
Danny Nothdurft <d.nothdurft@gmail.de>
2022-05-03 10:57:12 +0200

    Karten Confikuriert und Rückseite eingefügt. Bereit für die Annimation

--- app/src/components/Game.jsx
+++ app/src/components/Game.jsx
@@ -5,21 +5,33 @@ import game from '../data/game.json'
 
 // img
 import newRoundButton from '../img/newRoundButton.png'
+import cardBack from '../img/cardBack.png';
 
 function Game() {
 
-    const [ random, setRandom ] = useState()
+    const [ random, setRandom ] = useState(0)
 
     const play = () => {
         const generateRandom = Math.round( Math.random() * game.length - 0.5 );
+        console.log(game.length)
         setRandom(generateRandom)
+        console.log(generateRandom)
     }
+    console.log(random)
 
     return (
         <div className='game-component'>
             <div className='play-card'>
-                <p>{game[random]}</p>
+                <div className='back'>
+                    <img src={cardBack} alt="Karten Rückseite" />
+                </div>
+                <div className='front'>
+                    <p>{ game[random].text }</p>
+                    <hr />
+                    <img src={game[random].img} alt="Random img" />
+                </div>
             </div>
+            
             <button
                 className='game-button'
                 onClick={play}
